639928c1e3c6c1106f8702c753437611eb2fdaab
kami <kami-soft@yandex.ru>
2026-01-03 00:58:49 +0300

    up versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,16 @@ repositories {
 }
 
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.spring") version "2.3.0"
     id("org.springframework.boot") version "3.5.7"
     id("io.spring.dependency-management") version "1.1.7"
+}
+
+tasks.jar {
+    enabled = true
+}
+
+tasks.bootJar {
+    enabled = false
 }
--- mdc-wrapper-processing-starter/build.gradle.kts
+++ mdc-wrapper-processing-starter/build.gradle.kts
@@ -21,7 +21,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
 
 tasks.jar {
